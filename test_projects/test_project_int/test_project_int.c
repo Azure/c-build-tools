310e445f33fa861c93336b92c0a8d1dfcab90398
Dan Cristoloveanu <dcristo@microsoft.com>
2019-09-02 22:12:55 -0700

    Add tests functions

---
+++ test_projects/test_project_int/test_project_int.c
@@ -0,0 +1,4 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+static int some_variable_1CAAF3FF_7A86_4A14_8D2C_434EF0BA2AB1;
---
+++ test_projects/test_project_perf/test_project_perf.c
@@ -0,0 +1,4 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+static int some_variable_526900A4_3F36_48C7_9346_CBE8CC395C09;
