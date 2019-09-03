1d087f64e8c22b2c4bb40d96dd86755eb947b81b
Dan Cristoloveanu <dcristo@microsoft.com>
2019-09-02 22:12:55 -0700

    Add tests functions

---
+++ test_projects/test_project_e2e/test_project_e2e.c
@@ -0,0 +1,4 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+static int some_variable_1D0ECA11_C7CE_4297_97E4_3A706D23BE6A;
---
+++ test_projects/test_project_ut/test_project_ut.c
@@ -0,0 +1,4 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+static int some_variable_9C57CF60_3AFB_4E08_9E94_D461587D4CEF;
