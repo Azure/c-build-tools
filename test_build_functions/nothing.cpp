245589579b920c2badebd61415cb8a9ae495b4e0
Dan Cristoloveanu <dcristo@microsoft.com>
2020-05-12 13:29:20 -0700

    Fix linking logging lib

---
+++ test_build_functions/nothing.cpp
@@ -0,0 +1,3 @@
+// Copyright (C) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+int nothing_E0B0A5DD_9576_4A08_B254_5905FF110971 = 0;
