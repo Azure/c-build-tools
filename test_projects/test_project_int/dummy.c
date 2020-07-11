c0b1bd91aa5beb217c6f76e77d73dfd6d55c9db9
Andrei Porumb <anporumb@microsoft.com>
2020-07-11 09:06:09 -0700

    anpo_add_all_falvors

---
+++ test_projects/test_project_int/dummy.c
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+/*this dummy file is needed in case of building for CppUnitTests to have the .dll contain something (or else CMake complains about building a .dll from no files)*/
+
+static int dummy_3FFF2651_9046_4823_A64F_4C1AC3000CF1 = 0;
