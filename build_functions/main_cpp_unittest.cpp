d6400738478c8fada77935d21f1f786e74a4aedc
Dan Cristoloveanu <dcristo@microsoft.com>
2021-04-02 14:42:03 -0700

    Get the test suite name from the CMakeLists.txt

--- build_functions/main_cpp_unittest.cpp
+++ build_functions/main_cpp_unittest.cpp
@@ -4,4 +4,4 @@
 #include "run_cppunittest_suite.h"
 
 // test_suite_name is a define passed from the CMakeLists and identifies the test suite name
-RUN_TESTS_WITH_CPP_UNITTEST(test_suite_name)
+RUN_TESTS_WITH_CPP_UNITTEST(TEST_SUITE_NAME_FROM_CMAKE)
