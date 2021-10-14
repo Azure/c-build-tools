4019c5cfa6466d8d43e4d89fd9bb258e9a57e187
Parth Aggarwal <paaggarwal@microsoft.com>
2021-10-14 14:25:17 -0700

    improvements

---
+++ reals_check/test/reals/reals_dummy.h
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef REALS_DUMMY_H
+#define REALS_DUMMY_H
+
+#include "reals_check_test/dummy.h"
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+int reals_dummy_symbol_1();
+int reals_dummy_symbol_2();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // REALS_DUMMY_H
---
+++ reals_check/test/reals/reals_dummy_renames.h
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#define dummy_symbol_1 reals_dummy_symbol_1
+#define dummy_symbol_2 reals_dummy_symbol_2
