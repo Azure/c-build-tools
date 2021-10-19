7d72d2b5ac628d8a556c1cbc7789658b4aae8ad7
Parth Aggarwal <paaggarwal@microsoft.com>
2021-10-19 11:33:13 -0700

    removing old folder

--- real_check/test/both/both.c
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-#include "real_check_test/dummy.h"
-#include "real_dummy.h"
-
-int dummy_symbol_3(){
-    return 3;
-}
--- real_check/test/reals/real_dummy_renames.h
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-#define dummy_symbol_1 real_dummy_symbol_1
-#define dummy_symbol_2 real_dummy_symbol_2
