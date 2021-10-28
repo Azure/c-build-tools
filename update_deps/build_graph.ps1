2e4a2e698723467607481aa872dabe9f38493df1
Parth Aggarwal <paaggarwal@microsoft.com>
2021-10-28 11:29:07 -0700

    fix style

--- update_deps/build_graph.ps1
+++ update_deps/build_graph.ps1
@@ -1,6 +1,7 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+
 # parse repo URL to extract repo name
 function get-name-from-url {
     param (
@@ -10,6 +11,7 @@ function get-name-from-url {
    $split_by_dot = $split_by_slash[-1].Split('.') # $split_by_slash[-1] contains [repo_name].git
    return $split_by_dot[0] # $split_by_dot[0] contains [repo_name]
 }
+
 
 # get list of submodule URLs from a given repo URL
 function get-submodules {
@@ -45,18 +47,16 @@ function get-submodules {
     return $submodules
 }
 
+
 # dictionary to store mapping from repo name to level in dependency graph
 # root repo is level 0 and leaf repo is maximum level
 $repo_levels = New-Object -TypeName "System.Collections.Generic.Dictionary[string, int]"
-
 # queue to perform breadth-first search
 $queue = New-Object -TypeName "System.Collections.Queue"
-
 # list of repos to ignore while building graph
 $repos_to_ignore = "libcrc", "smhasher", "vcpkg", "mimalloc", "jemalloc", "Azure-Messaging-Metrics"
 
 function Build-Graph {
-
     # get front of queue
     $repo_url = $queue.Dequeue()
     $repo_name = get-name-from-url -url $repo_url
@@ -65,7 +65,6 @@ function Build-Graph {
     if(-not $repo_levels.ContainsKey($repo_name)) {
         $repo_levels[$repo_name] = 0
     }
-    
     # clone repo if not already present
     if(-not (Test-Path -Path $repo_name)) {
         git clone $repo_url
@@ -74,7 +73,6 @@ function Build-Graph {
     $repo_level = $repo_levels[$repo_name]
     # get list for submodules URLs 
     $submodules = get-submodules $repo_url
-
     # iterate of list of submodules 
     foreach($submodule in $submodules) {
         $submodule_name = get-name-from-url -url $submodule
@@ -83,24 +81,21 @@ function Build-Graph {
         if ($submodule_name -in $repos_to_ignore) {
             continue
         }
-
         $level = 0
         [void]$repo_levels.TryGetValue($submodule_name, [ref]$level)
-
         # update repo level of submodule if path from current repo is longer
         if (($repo_level+1) -gt $level) {
             $repo_levels[$submodule_name] = $repo_level+1
         }
-
         # add submodule to queue
         $queue.Enqueue($submodule)
     }
-
     # if queue is not empty, make recursive call
     if ( $queue.Count -ne 0) {
         Build-Graph
     }
 }
+
 
 # seed queue with given argument
 $queue.Enqueue($args[0])
@@ -111,12 +106,10 @@ Build-Graph
 $repo_levels_list = [Linq.Enumerable]::ToList($repo_levels)
 # sort list by descending order of level
 $repo_levels_list.Sort({$args[1].Value.CompareTo($args[0].Value)})
-
 # create list to hold repos in order to be updated
 $repo_order = New-Object -TypeName "System.Collections.ArrayList"
-
+# collect repo names in repo_order
 $repo_levels_list.ForEach({$repo_order.Add($args[0].Key)})
-
 Write-Host $repo_order
 
 <#
