781bf0abc59a4d4fac1544d691a557acf89e2713
Dan Cristoloveanu <dcristo@microsoft.com>
2024-01-17 19:41:25 -0800

    Move Sarif results checker to c-build-tools

---
+++ sarif_results_checker/Options.cs
@@ -0,0 +1,27 @@
+﻿// //------------------------------------------------------------
+// // Copyright (c) Microsoft Corporation.  All rights reserved.
+// //------------------------------------------------------------
+
+namespace Azure.Messaging.SarifResultsChecker
+{
+    using CommandLine;
+
+    internal class Options
+    {
+        internal const string DefaultSarifPath = ".";
+        internal const string DefaultIgnorePaths = "";
+
+        [Option('f', "sarifPath", Default = DefaultSarifPath, HelpText = "Path where Sarif files to be checked exist.")]
+        public string SarifPath
+        {
+            get;
+            set;
+        }
+        [Option('i', "ignorePaths", Default = DefaultIgnorePaths, HelpText = "Paths to folders containing files to be ignored during the check (semicolon delimited).")]
+        public string IgnorePaths
+        {
+            get;
+            set;
+        }
+    }
+}
