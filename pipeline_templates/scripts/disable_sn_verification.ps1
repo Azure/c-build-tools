4a175afd0a88ea51db238e99c08d05c67a5340b7
Dan Cristoloveanu <dcristo@microsoft.com>
2024-01-22 22:08:30 -0800

    Add compiler generated files and disable_sn_verification tool (#167)
    * Add VS_GLOBAL_CompilerGeneratedFilesOutputPath
    
    * Add SN verification disable task

---
+++ pipeline_templates/scripts/disable_sn_verification.ps1
@@ -0,0 +1,4 @@
+#Copyright (C) Microsoft Corporation. All rights reserved.
+
+reg ADD "HKLM\Software\Microsoft\StrongName\Verification\*,*" /f
+reg ADD "HKLM\Software\Wow6432Node\Microsoft\StrongName\Verification\*,*" /f
