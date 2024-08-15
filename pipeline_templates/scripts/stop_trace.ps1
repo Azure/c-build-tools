b1f2f0fcd9faa64d4d31644063e706fbf089fe2d
Andrei Porumb <anporumb@microsoft.com>
2024-08-15 14:53:49 -0700

    make the same changes to sto_trace as in start_trace

--- pipeline_templates/scripts/stop_trace.ps1
+++ pipeline_templates/scripts/stop_trace.ps1
@@ -21,8 +21,33 @@ try
 {
     # print the current state first
     logman
-    # then stop
+
+
+    # then stop or reboot machine if it doesn't complete in 10 seconds
+    Write-Output "Stopping any old trace (or reboot if it doesn't stop) ..."
+
+    $timeout = 10000 # 10 seconds
+
+    # Setup a timer
+    $timer = New-Object System.Timers.Timer
+    $timer.Interval = $timeout
+    $timer.AutoReset = $false
+
+    # Define the action to take when the timer elapses
+    $timerEvent = Register-ObjectEvent -InputObject $timer -EventName Elapsed -Action {
+        Write-Host "Timeout reached. Restarting the machine! Expect the build to (eventually) fail... "
+        Restart-Computer -Force
+    }
+
+    # Start the timer
+    $timer.Start()
+
+    # timer watches over logman stop... 
     logman stop testgate
+
+    # If we get here, that means the event was not fired, that can only mean that logman stopped naturally. Undo the watchdog.
+    Unregister-Event -SourceIdentifier $timerEvent.Name
+    $timer.Dispose()
 }
 catch
 {
