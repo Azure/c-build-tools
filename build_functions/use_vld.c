6d1b4b43d7fd339d3f32ac9b0ada4f4374687d81
Andrei Porumb <anporumb@microsoft.com>
2024-09-25 11:49:58 -0700

    added use_vld.c and insert it in all .exe targets that are not build from .cs files

---
+++ build_functions/use_vld.c
@@ -0,0 +1,7 @@
+
+// Copyright (C) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "vld.h"
+
+static int not_used=0; /*prevent compiles from complaining about empty file*/
