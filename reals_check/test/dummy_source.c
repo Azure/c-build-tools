8ff548abaca8a13e3991d5b0a2ca194b9cf44e00
anporumb <anporumb@users.noreply.github.com>
2025-01-07 11:33:44 -0800

    use vld is now a lib (#218)
    * use_vld is now a lib
    
    * don't print message every link

--- reals_check/test/dummy_source.c
+++ reals_check/test/dummy_source.c
@@ -1,2 +1,5 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+
+static int avoid_C4206_nonstandard_extension_used_translation_unit_is_empty =0;
