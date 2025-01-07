f681e09f1b13450f2c8c61c1b4e0782fc397f5db
Andrei Porumb <anporumb@microsoft.com>
2025-01-07 10:59:43 -0800

    order is important

--- build_functions/nothing.c
+++ build_functions/nothing.c
@@ -1,1 +1,4 @@
-static int t = 0;
+// Copyright (C) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+static int nothing_C894921E_51A0_48B5_8052_16CDEE0E3C24 = 0;
