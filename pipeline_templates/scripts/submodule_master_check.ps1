16aa5702dd20d94924e40ff22b55616b79b924a2
Dan Cristoloveanu <dcristo@microsoft.com>
2025-07-16 09:51:15 -0700

    Add custom submodule branches option

--- pipeline_templates/scripts/submodule_master_check.ps1
+++ pipeline_templates/scripts/submodule_master_check.ps1
@@ -3,13 +3,19 @@
 #this script checks for every submodule of the current repo if its SHA is an ancestor of SHA at the current the master branch of the submodule
 
 Param(
-    $ignoreSubmodules = ""
+    # A comma-separated list of submodule paths to ignore when performing the check
+    $ignoreSubmodules = "",
+    # Custom branches to check against instead of the default master branch, can be specified as a comma-separated list in format "submodulePath:branchName"
+    # Example: "deps/submodule1:main,deps/submodule2:develop"
+    $customSubmoduleBranches = "",
 )
 
 # Initialize return code to 0
 $global:returnCode = 0
 
 $ignoreSubmoduleParts = $ignoreSubmodules -split ','
+
+$customSubmoduleBranchParts = $customSubmoduleBranches -split ','
 
 # Iterate over each submodule
 $submodules = git submodule foreach --quiet 'echo $name $sha1'
@@ -18,10 +24,24 @@ foreach ($submodule in $submodules) {
     $submodulePath = $parts[0]
     $submoduleSHA = $parts[1]
 
-    if ($ignoreSubmoduleParts -contains $submodulePath)
-    {
+    # Check if the submodule path is in the ignore list
+    if ($ignoreSubmoduleParts -contains $submodulePath) {
         Write-Host "Ignoring submodule $submodulePath"
         continue
+    }
+
+    # Determine the branch to check against
+    $branchToCheck = "master"  # Default branch
+    foreach ($customBranch in $customSubmoduleBranchParts) {
+        $branchParts = $customBranch -split ':'
+
+        # Check if the submodule path matches the custom branch definition
+        if ($branchParts[0] -eq $submodulePath) {
+            if ($branchParts.Length -gt 1){
+                $branchToCheck = $branchParts[1]
+            }
+            break
+        }
     }
 
     # Navigate to the submodule directory
@@ -30,25 +50,29 @@ foreach ($submodule in $submodules) {
     # Fetch the latest changes from the remote repository
     git fetch origin
 
+    # compute ref
+    $ref = "refs/remotes/origin/$branchToCheck"
+    $originBranch = "origin/$branchToCheck"
+
     # Check if the master branch exists
-    git show-ref --verify --quiet refs/remotes/origin/master
+    git show-ref --verify --quiet $ref
 
     # Capture the exit code
     $exitCode = $LASTEXITCODE
 
     if ($exitCode -gt 0) {
-        Write-Host "Master branch not found in $submodulePath, skipping..."
+        Write-Host "$branchToCheck branch not found in $submodulePath, skipping..."
     } else {
         # Check if the SHA is an ancestor of the master branch
-        git merge-base --is-ancestor $submoduleSHA origin/master
+        git merge-base --is-ancestor $submoduleSHA $originBranch
 
         $isAncestor = $LASTEXITCODE -eq 0
 
         if ($isAncestor) {
-           Write-Host "SHA $submoduleSHA found in master branch of $submodulePath"
+           Write-Host "SHA $submoduleSHA found in $branchToCheck branch of $submodulePath"
         } else {
-            
-            Write-Host "SHA $submoduleSHA not found in master branch of $submodulePath"
+
+            Write-Host "SHA $submoduleSHA not found in $branchToCheck branch of $submodulePath"
             $global:returnCode = 1
         }
     }
